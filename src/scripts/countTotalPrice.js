d330efa6cbb4d3e522385c34a10c9200788b9b0b
Mexty <m.harchenko@i.ua>
2025-02-24 22:06:38 +0200

    Added counter

--- src/scripts/countTotalPrice.js
+++ src/scripts/countTotalPrice.js
@@ -1,18 +1,20 @@
 import fs from 'node:fs/promises';
 import { DB_PATH } from '../constants/index.js';
 
-export const modifyProducts = async () => {
+export const countTotalPrice = async () => {
   try {
     const productsData = await fs.readFile(DB_PATH, 'utf-8');
     const products = JSON.parse(productsData);
 
-    const modifiedProducts = products.map(
-      ({ description, ...product }) => product
+    const totalPrice = products.reduce(
+      (acc, { price }) => acc + Number(price),
+      0
     );
-    await fs.writeFile(DB_PATH, JSON.stringify(modifiedProducts, undefined, 2));
+
+    console.log('Сумарна вартість усіх позицій -', totalPrice.toFixed(2));
   } catch (error) {
     console.log(error);
   }
 };
 
-modifyProducts();
+countTotalPrice();
